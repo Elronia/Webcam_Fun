8ee2d563068347cd38f9f3192b445e9ca33b1f0c
Elronia <zar.catherine@gmail.com>
2021-04-05 00:37:58 -0400

    taking the data out of the canvas and adding the download attribute

--- scripts.js
+++ scripts.js
@@ -30,8 +30,17 @@ function paintToCanvas() {
 }
 
 function takePhoto() {
+    // played the sound
     snap.currentTime = 0;
     snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'beautiful');
+    link.innerHTML = `<img src="${data}" alt="Beautiful Girl"/>`;
+    strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
